0ccf80f4b82dd39435f3ca46d9de265b143a0da8
dbarili <dantebarili1@gmail.com>
2024-08-19 13:43:41 -0300

    + (falta funcion avanzatiempoAst)

--- calculos.cpp
+++ calculos.cpp
@@ -39,5 +39,36 @@ void avanzaTiempoCuerpos(OrbitalSim* sim){
 
 void avanzaTiempoAsteroides(OrbitalSim* sim){
 
+    int i, j;
+    long double norma;
+    Vector3 vUnitario;
+
+    for(i = 0; i < sim->cantidadAsteroides; i++){    
+
+        Vector3 ai = {0,0,0};
+        for(j = 0; j < sim->cantidadAsteroides; j++){  
+
+            if (i == j){
+                continue;
+            }
+            
+            norma = (long double)(Vector3Length(Vector3Subtract(sim->cuerposCel[i]->posicion, sim->cuerposCel[j]->posicion)));
+            
+            if (norma!=0){
+                vUnitario = Vector3Scale(Vector3Subtract(sim->cuerposCel[i]->posicion, sim->cuerposCel[j]->posicion), 1/norma);
+            }
+
+            ai = Vector3Add(ai, Vector3Scale(vUnitario, (-1) * G * sim->cuerposCel[j]->masa * (1/(norma*norma))));
+
+        }
+
+        sim->cuerposCel[i]->velocidad = Vector3Add(sim->cuerposCel[i]->velocidad, Vector3Scale(ai, sim->timeStep)); 
+
+    }
+    
+    for(i=0;i<sim->cantidadAsteroides;i++){
+        sim->cuerposCel[i]->posicion = Vector3Add(sim->cuerposCel[i]->posicion, Vector3Scale(sim->cuerposCel[i]->velocidad, sim->timeStep));
+    }
+
     return;
 }
